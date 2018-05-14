b4b8d0eea0cf74866bab1ea4a2af355d8b8f69cd
YogeshGautam48 <yogeshgautam@yahoo.com>
2018-05-14 08:12:37 +0545

    Done Some Validation and Improvement in Some Views

--- BO/BOAvailableTiming.cs
+++ BO/BOAvailableTiming.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,17 @@ namespace BO
     public class BOAvailableTiming
     {
         public int Id { get; set; }
+
+        [Required]
         public Nullable<System.DateTime> Date { get; set; }
+
+        [Required]
         public Nullable<System.TimeSpan> StartTime { get; set; }
+
+        [Required]
         public Nullable<System.TimeSpan> EndTime { get; set; }
+
+        [Required]
         public Nullable<bool> IsAvailable { get; set; }
         public Nullable<int> VipId { get; set; }
     }
